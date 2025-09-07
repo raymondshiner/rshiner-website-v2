27aeb30767359411e80c9a48b1f80b0da24f3a9f
Raymond Shiner <raymondshiner@gmail.com>
2025-09-06 17:38:31 -0700

    vitest installed

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest/config" />
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    test: {
+        // ... Specify options here.
+    },
+});
