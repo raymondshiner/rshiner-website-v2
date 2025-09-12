04c50e28d733680a271cef7acaec2d4d7f348d3d
Raymond Shiner <raymondshiner@gmail.com>
2025-09-12 09:10:17 -0700

    styling done

--- src/sections/About/About.tsx
+++ src/sections/About/About.tsx
@@ -1,3 +1,8 @@
 export default function About() {
-  return <div>About Me</div>
+  return <div className="container mx-auto p-4 flex flex-col justify-center items-center h-full">
+    <div className="text-5xl font-bold underline">
+      About Me
+    </div>
+  </div>
+
 }
